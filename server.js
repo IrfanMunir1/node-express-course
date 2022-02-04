7fc75cf8c948cdf64dd36cc1d113da235025c65a
IrfanMunir1 <irfan.munir@algo.com>
2022-02-04 20:21:57 +0500

    add login post route

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
 const mockUserData=[
 {name:"Bill"},
 {name:"Jill"}
@@ -28,5 +32,29 @@ app.get('/users/:id',function(req,res){
 
 })
 
+app.post('/login',function(req,res){
+    
+    console.log(req.body);
+    const username=req.body.username;
+    const password=req.body.password;
+
+    console.log(req.body.username);
+    console.log(req.body.password);
+    const mockUsername="billyTheKid";
+    const mockPassword="superSecret";
+
+    if (username===mockUsername && password===mockPassword){
+         res.json({
+              success: true,
+              message: 'password and username match!',
+              token: 'encrypted token goes here'
+         })
+    } else {
+         res.json({
+              success: false,
+              message: 'password and username do not match'
+         })
+    }
+})
 
 app.listen(8000,function(){console.log('server is listening')})
